6c117ff29d550a856845ad77f46323a207e97090
kaphooligan <thedud1235@gmail.com>
2018-04-23 11:23:25 -0400

    Movie Class

---
+++ Movie.java
@@ -0,0 +1,53 @@
+
+public class Movie {
+	//instance variables
+	private String name;
+	private int YearRealeased;
+	private String director;
+	//default constructor
+	public Movie() {
+		name = "";
+		YearRealeased = 1890;
+		director = "";
+	}
+	
+	//non-static methods
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String newDirector) {
+		director =newDirector;
+	}
+	
+	public void setYearRealeased(int newYearRealeased) {
+		if(newYearRealeased >=1890) {
+			YearRealeased = newYearRealeased;
+		}
+	}
+	
+	public int getYearReleased() {
+		return YearRealeased;
+	}
+	
+	public boolean equals(Movie otherMovie) {
+		boolean isEqual = false;
+		if(this.name.equals(otherMovie.name) && 
+				(this.YearRealeased == otherMovie.YearRealeased)&&this.director.equals(otherMovie.director)) {
+			isEqual = true;
+		}
+		
+		return isEqual;
+	}
+	
+	
+	
+	
+}
