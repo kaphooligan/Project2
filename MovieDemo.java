6a95988ab3705dc57b3204c14a9b2308d05f7570
kaphooligan <thedud1235@gmail.com>
2018-04-24 10:40:53 -0400

    movie demo still needs to print to file and read from file

---
+++ MovieDemo.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+public class MovieDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		Movie [] MovieArray = new Movie[10];
+		int numberOfMovies = 0;
+		int option;
+		
+		do {
+			System.out.println("Press 1 to add a Movie");
+			System.out.println("Press 2 to look at all the existing Movies");
+			System.out.println("Press 3 to end the program");
+			option = keyboard.nextInt();
+			keyboard.nextLine(); // go to the next line
+			
+			if(option == 1) {
+				System.out.println("What is the name of the movie?");
+				String movieName = keyboard.nextLine();
+				System.out.println("Who is the director?");
+				String directorName = keyboard.nextLine();
+				System.out.println("What year was this movie released?");
+				int yearReleased = keyboard.nextInt();
+				
+				Movie currentMovie = new Movie();
+				currentMovie.setName(movieName);
+				currentMovie.setYearRealeased(yearReleased);
+				currentMovie.setDirector(directorName);
+				if(numberOfMovies < MovieArray.length) {
+					
+					boolean duplicate = false;
+					int index = 0;
+					while(!duplicate && index < numberOfMovies) {
+						if(MovieArray[index].equals(currentMovie)) {
+							duplicate = true;
+						}
+						
+						index++;
+					}
+					
+					if(!duplicate) {
+						MovieArray[numberOfMovies] = currentMovie;
+						numberOfMovies++;
+					}
+					else {
+						System.out.println("This building already exists!");
+					}
+				}
+				else {
+					System.out.println("There is no more room for additional buildings");
+				}
+			}
+			else if(option == 2) {
+				for(int index = 0; index < numberOfMovies; index++) {
+					System.out.println("Name: " +  MovieArray[index].getName());
+					System.out.println("Director: " + MovieArray[index].getDirector());
+					System.out.println("Year Released: " + MovieArray[index].getYearReleased());
+				}
+			}
+			else if(option == 3) {
+				System.out.println("Thank you for using the program!");
+			}
+			else {
+				System.out.println("Error! Invalid option! Try again!");
+			}
+			
+		}while(option != 3);
+		
+	}
+
+}
